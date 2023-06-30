aec43461d9db165a201f32c45705891dbf47ca8b
vicky <vigmurug@gmail.com>
2023-06-30 06:54:39 +0530

    comments updated

--- week4-smart-contract-winner/contracts/Winner.sol
+++ week4-smart-contract-winner/contracts/Winner.sol
@@ -1,16 +1,20 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+// interface of the Alchemy's Contract
 interface Contract {
     function attempt() external;
 }
 
 contract Winner {
-    address contractAddress;
+    address contractAddress; // variable storing Alchemy's Contract address to call
 
+    // initializing my contract with the Alchemy's Contract address
     constructor(address _contractAddress){
         contractAddress = _contractAddress;
     }
+
+    // calling the attempt() of Alchemy's Contract from my Winner contract
     function callWinner() external{
         return Contract(contractAddress).attempt();
     }
--- week4-smart-contract-winner/scripts/deploy.js
+++ week4-smart-contract-winner/scripts/deploy.js
@@ -2,17 +2,21 @@
 const hre = require('hardhat');
 
 async function main () {
+
+    // address of the Contract to make function call
     const contractAddress = "0xcF469d3BEB3Fc24cEe979eFf83BE33ed50988502";
 
+    // Winner contract deployment part
     const EmitWinner = await hre.ethers.getContractFactory("Winner");
-    const emitWinner = await EmitWinner.deploy(contractAddress);
+    const emitWinner = await EmitWinner.deploy(contractAddress); // initializing constructor with Contract address
 
     await emitWinner.waitForDeployment();
     console.log(`Contract deployed at ${emitWinner.target}`);
+    // Winner contract deployed at 0xA663C2624463944Dcc98Cd71F748A1Cf2d367B64
 
     const result = await emitWinner.callWinner();
     console.log(result);
-
+    // event emitted tx hash - 0xf03f6b41498b2b5bbde6740d41417361a748535da80040ff931cbe5aa314899a
 }
 
 main().catch((error) => {
